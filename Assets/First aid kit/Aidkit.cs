ec140efe5fcf4449bcde99165abccca882f1b8ff
tyanecaxa <kudashkina.2008@gmail.com>
2024-03-14 17:53:21 +0700

    First Aid Kit

---
+++ Assets/First aid kit/Aidkit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aidkit : MonoBehaviour
+{
+    public float healAmount = 50;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+        if(playerHealth != null )
+        {
+            playerHealth.AddHealth( healAmount );
+        }
+        Destroy( gameObject );
+    }
+    
+}
